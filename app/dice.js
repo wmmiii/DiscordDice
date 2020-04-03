b773a3b16cf235004f43e496c16271730cef401f
William Martin <williammartinthird@gmail.com>
2020-04-02 18:56:12 -0600

    Added complex parsing for dice expressions.
    You can now request complex expressions from the bot such as
    "/roll 8d6 + 2 - 4" and it will evaluate them for you.
    
    All other behavior should remain the same including "/roll" and
    "/roll 20".
    
    Cleaned up README.

--- app/dice.js
+++ app/dice.js
@@ -2,6 +2,7 @@ var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 const db = require('./db');
+var parser = require('./parser');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -9,6 +10,8 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 
+// The user id of the last 20 users that posted an invalid request.
+const shitlist = [];
 
 // Initialize Discord Bot
 var bot = new Discord.Client({
@@ -24,17 +27,25 @@ bot.on('ready', function (evt) {
 
 bot.on('message', function (user, userId, channelId, message, evt) {
   if (message.substring(0, 5) == '/roll') {
-    const param = message.substring(1).split(' ').splice(1)[0];
-    if (param) {
+    const params = message.split(' ');
+    params.shift(); // Remove command string.
+    if (params.length == 1) {
+        const param = params[0];
       if (param === "help" || param === "?" || param === "") {
         sendHelp(channelId);
       } else  if (param === "average" || param === "me" || param === "myAverage") {
         getAverage(user, userId, channelId);
       } else {
-        roll(user, userId, channelId, message, parseInt(param));
+        if (/^\d+$/i.test(param)) {
+            roll(user, userId, channelId, message, parseInt(param));
+        } else {
+            parseAndRoll(user, userId, channelId, message.substring(6));
+        }
       }
+    } else if (params.length == 0) {
+        roll(user, userId, channelId, message);
     } else {
-      roll(user, userId, channelId, message);
+        parseAndRoll(user, userId, channelId, message.substring(6));
     }
   }
 });
@@ -57,16 +68,83 @@ function roll(user, userId, channelId, message, sides=6) {
     const roll = Math.floor(Math.random() * Math.floor(sides) + 1);
     logger.info(user + '(' + userId + ') rolled a ' + sides + ' sided die');
     logger.info('outcomeoutcome: ' + roll);
-    bot.sendMessage({
-      to: channelId,
-      message: 'You rolled a: ' + roll
-    });
-    db.postRoll(user, userId, channelId, message, sides, roll, (err) => {
-      if (err) {
-        logger.error(err);
-      }
-    });
+    reportRoll(roll, user, userId, channelId, message, sides, roll);
   }
+}
+
+/**
+ * Given more than one argument parses the arguments and returns the solution.
+ * 
+ * @param {string} user The username of the user who requested the roll.
+ * @param {string} userId The id of the user who requested the roll.
+ * @param {string} channelId The id of the channel that the message was posted to.
+ * @param {string} params The tokens of the roll request.
+ */
+function parseAndRoll(user, userId, channelId, message) {
+    try {
+        const roll  = parser.roll(message);
+        reportRoll(roll, user, userId, channelId, message, roll);
+    } catch (e) {
+        shitlist.push(userId);
+        while (shitlist.length > 20) {
+            shitlist.shift();
+        }
+
+        if (userId == "221780469371371520") {
+            bot.sendMessage({
+                to: channelId,
+                message: 'Fuck off Joey. ' + e.message,
+            });
+        } else if (shitlist.filter(user => user === userId).length > 5) {
+            bot.sendMessage({
+                to: channelId,
+                message: 'Seriously ' + user + '? ' + e.message,
+            });
+        } else {
+            bot.sendMessage({
+                to: channelId,
+                message: 'I couldn\'t figure out how to roll that: ' + e.message,
+            });
+        }
+    }
+}
+
+/**
+ * Reports the roll back to the user who requested it and records the result of the roll.
+ * 
+ * @param {number} roll The result of the roll.
+ * @param {string} user The username of the user who requested the roll.
+ * @param {string} userId The id of the user who requested the roll.
+ * @param {string} channelId The id of the channel that the message was sent to.
+ * @param {string} message The body of the roll request.
+ * @param {number|undefined} sides The number of sides of the dice that was rolled
+ *                                 or undefined if an advanced expression was used.
+ */
+function reportRoll(roll, user, userId, channelId, message, sides) {
+    if (roll === 420 && 1 > Math.random() * 20)  {
+        bot.sendMessage({
+            to: channelId,
+            message: 'You rolled a: ' + roll + ' blaze it.'
+        });
+
+    } else if (roll === 69 && 1 > Math.random() * 20)  {
+        bot.sendMessage({
+            to: channelId,
+            message: 'You rolled a: ' + roll + ' Nice.'
+        });
+    } else {
+        bot.sendMessage({
+            to: channelId,
+            message: 'You rolled a: ' + roll
+        });
+    }
+    if (sides != null) {
+        db.postRoll(user, userId, channelId, message, sides, roll, (err) => {
+            if (err) {
+            logger.error(err);
+            }
+        });
+    }
 }
 
 function getAverage(user, userId, channelId) {
@@ -119,6 +197,7 @@ function sendHelp(channelId) {
     to: channelId,
     message: 'Welcome to Discord Dice!\nI\'m a bot, beep boop!\n\nYou can invoke me by typing "/roll"\
         \nIf you type a number after /roll like "/roll 10" you can roll a custom sided dice!\
+        \nOr, if you want to get real fancy you can do something like "/roll 8d6 + 4", fancy!\
         \nIf you type "/roll average" you can see statistics on your rolls!'
   });
 }
